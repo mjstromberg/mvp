e83a3c6404d928a277c9b9332f2f47ed5628a423
Hack Reactor Students <accounts@hackreactor.com>
2016-10-17 11:08:29 -0700

    Add homepage route to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+// add routes here
+app.get('/', function(req, res) {
+  res.send('homepage');
+});
+
 app.listen(8000);
 
 module.exports = app;
