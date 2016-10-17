e148936bd8df46e2b871f12480c6a34ed9cf2b17
Hack Reactor Students <accounts@hackreactor.com>
2016-10-17 12:23:41 -0700

    Add favicon middleware, needs debugging

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var favicon = require('serve-favicon');
+
 var app = express();
 var port = 8000;
+
+// middleware
+app.use(favicon(__dirname + '/assets/favicon.ico'));
 
 // add routes here
 app.get('/', function(req, res) {
