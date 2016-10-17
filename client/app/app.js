b10755c5d4eda76d116ff442998783229b2ae05b
Hack Reactor Students <accounts@hackreactor.com>
2016-10-17 12:31:28 -0700

    Add landlord route and empty template and controller

--- client/app/app.js
+++ client/app/app.js
@@ -2,3 +2,11 @@ var rentocracy = angular.module('rentocracy', [
   'ngRoute'
 ]);
 
+rentocracy.config(function($routeProvider) {
+  $routeProvider
+    .when('/landlords', {
+      template: 'landlords/landlords.html',
+      controller: 'landlordsCtrl'
+    })
+    .otherwise('/');
+});
