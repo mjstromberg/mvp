f27c6d262fdea0c962492847b12dd76d68a3056d
Hack Reactor Students <accounts@hackreactor.com>
2016-10-18 10:52:37 -0700

    Add default landlord profile pic and get two-way data binding to work

--- client/app/landlords/landlords.js
+++ client/app/landlords/landlords.js
@@ -8,11 +8,36 @@ angular.module('rentocracy.landlords', [])
       async: false,
       success: function(data, status, jqXHR) {
         console.log('client data: ', data);
+        $scope.searchResults = data;
       },
       error: function(jqXHR, status, error) {
         console.log('client error: ', error);
       }
     })
+  };
 
-  }
+  $scope.isFeatured = true;
+  $scope.profileTitle = $scope.isFeatured ? 'Featured' : 'Search Results';
+  
+  $scope.featuredProfile = {
+    first_name: 'Fakey',
+    last_name: 'McLandlord',
+    stars: 3,
+    review_count: 1,
+    reviews: [
+      {
+        username: 'satisfied_renter9000',
+        review_text: 'Fakey McLandlord is the best!',
+        date_start: '2010-05-01',
+        date_end: '2011-05-01',
+        stars_landlord: 3
+      }
+    ]
+  };
+
+  $scope.searchResults = [];
+
+  $scope.currentData = $scope.isFeatured ?
+    [$scope.featuredProfile] :
+    $scope.searchResults;
 }]);
